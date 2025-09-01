bd06b63d73f1c660748b797e5428810aff20c037
fontaine <33033694+DavidFountain@users.noreply.github.com>
2025-09-01 13:38:07 +0000

    updates

---
+++ dbtlearn/tests/consistent_created_at.sql
@@ -0,0 +1,8 @@
+SELECT
+    *
+FROM
+    {{ref("fct_reviews")}} rev
+LEFT JOIN
+    {{ref("dim_listings_cleansed")}} lis
+    ON rev.listing_id = lis.listing_id
+WHERE rev.review_date < lis.created_at
